24d3b576add74ad4c2215481b9f8ba73c2230a76
Pavanelli99 <pavanelli1999@gmail.com>
2022-12-05 21:48:43 -0400

    criando projeto

---
+++ Arvores_Banco/comandos/comados.sql
@@ -0,0 +1,32 @@
+
+CREATE DATABASE Arvores
+
+create table 
+tb_node(
+   id int not null auto_increment,
+   username varchar(255),
+   password varchar(255),
+   status int,
+   token double,
+   primary key(id)
+   );
+
+
+
+create table 
+tb_car(
+   id int not null auto_increment,
+   name varchar(255),
+   model varchar(255),
+   dt_Fabrication date,
+   value double,
+   primary key(id)
+   );
+
+--describe tb_user
+
+
+SELECT * FROM tb_user
+
+
+
